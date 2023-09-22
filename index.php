b74a2a1b511c98439469a2c1048da49421bab22a
edward2287 <119518465+edward2287@users.noreply.github.com>
2023-09-22 00:11:47 -0400

    arreglos en carrito de compras listo

--- index.php
+++ index.php
@@ -47,17 +47,12 @@ $con = $db->conectar();
     <header class="header">
         <nav class="nav container">
             <div class="nav__data">
-                <a href="./index.php" class="nav__logo">
-                    <img src="./recursos/SVGLogo.svg" alt="">
-                </a>
-                <!--=============== menu hamburguesa ===============-->
-                <div class="nav__toggle" id="nav-toggle">
-                    <i class="ri-menu-line nav__burger"></i>
-                    <i class="ri-close-line nav__close"></i>
-                </div>
-                <!--=============== Carro de compras responsive ===============-->
-                <div class="nav__toggle nav__toggless" id="nav-toggle">
-                    <a href="./agregarcarrito.php">
+                <section class="nav__logo_cart">
+                    <a href="./index.php" class="nav__logo">
+                        <img src="./recursos/SVGLogo.svg" alt="">
+                    </a>
+
+                    <a href="./agregarcarrito.php" class="carrito ">
                         <svg style="fill: #1c3a6b;" xmlns="http://www.w3.org/2000/svg" height="1.5em" viewBox="0 0 576 512">
                             <path d="M24 0C10.7 0 0 10.7 0 24S10.7 48 24 48H69.5c3.8 0 7.1 2.7 7.9 6.5l51.6 271c6.5 34 36.2 58.5 70.7 58.5H488c13.3 0 24-10.7 24-24s-10.7-24-24-24H199.7c-11.5 0-21.4-8.2-23.6-19.5L170.7 288H459.2c32.6 0 61.1-21.8 69.5-53.3l41-152.3C576.6 57 557.4 32 531.1 32H360V134.1l23-23c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-64 64c-9.4 9.4-24.6 9.4-33.9 0l-64-64c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l23 23V32H120.1C111 12.8 91.6 0 69.5 0H24zM176 512a48 48 0 1 0 0-96 48 48 0 1 0 0 96zm336-48a48 48 0 1 0 -96 0 48 48 0 1 0 96 0z" />
                         </svg>
@@ -70,7 +65,15 @@ $con = $db->conectar();
                         <?php } ?>
                     </a>
 
+                </section>
+
+                <!--=============== menu hamburguesa ===============-->
+                <div class="nav__toggle" id="nav-toggle">
+                    <i class="ri-menu-line nav__burger"></i>
+                    <i class="ri-close-line nav__close"></i>
                 </div>
+                <!--=============== Carro de compras responsive ===============-->
+
             </div>
 
             <!--=============== NAV MENU ===============-->
@@ -225,22 +228,9 @@ $con = $db->conectar();
 
                 <!--=============== carro de compras desktop ===============-->
 
-                <li>
-                    <a href="./agregarcarrito.php" class="nav__link nav__link_cart">
-                        <svg style="fill: #1c3a6b;" xmlns="http://www.w3.org/2000/svg" height="1.5em" viewBox="0 0 576 512">
-                            <path d="M24 0C10.7 0 0 10.7 0 24S10.7 48 24 48H69.5c3.8 0 7.1 2.7 7.9 6.5l51.6 271c6.5 34 36.2 58.5 70.7 58.5H488c13.3 0 24-10.7 24-24s-10.7-24-24-24H199.7c-11.5 0-21.4-8.2-23.6-19.5L170.7 288H459.2c32.6 0 61.1-21.8 69.5-53.3l41-152.3C576.6 57 557.4 32 531.1 32H360V134.1l23-23c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-64 64c-9.4 9.4-24.6 9.4-33.9 0l-64-64c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l23 23V32H120.1C111 12.8 91.6 0 69.5 0H24zM176 512a48 48 0 1 0 0-96 48 48 0 1 0 0 96zm336-48a48 48 0 1 0 -96 0 48 48 0 1 0 96 0z" />
-                        </svg>
-                        <?php if ($num_cart > 0) { ?>
-                            <span id="num_cart" class="badge bg-secondary carritosss">
-                                <?php echo $num_cart; ?>
-                            </span>
-                        <?php } else { ?>
-                            <span id="num_cart" class="badge bg-secondary carritosss"></span>
-                        <?php } ?>
-                    </a>
-                </li>
                 </ul>
             </div>
+
         </nav>
     </header>
 
