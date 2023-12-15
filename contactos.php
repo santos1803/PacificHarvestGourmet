233e6b1382ca04471abb00184f467843d0960f0f
Elias Ramos <eliasrk24@gmail.com>
2023-12-15 19:36:20 -0400

    se quitaron productos

--- contactos.php
+++ contactos.php
@@ -133,7 +133,7 @@ $resultado = $sql->fetchAll(PDO::FETCH_ASSOC);
                     <div class="nav__link">
                         <svg style="fill: #1c3a6b; margin-right: 7px" xmlns="http://www.w3.org/2000/svg" height="1.2em" viewBox="0 0 512 512">
                             <path d="M64 144a48 48 0 1 0 0-96 48 48 0 1 0 0 96zM192 64c-17.7 0-32 14.3-32 32s14.3 32 32 32H480c17.7 0 32-14.3 32-32s-14.3-32-32-32H192zm0 160c-17.7 0-32 14.3-32 32s14.3 32 32 32H480c17.7 0 32-14.3 32-32s-14.3-32-32-32H192zm0 160c-17.7 0-32 14.3-32 32s14.3 32 32 32H480c17.7 0 32-14.3 32-32s-14.3-32-32-32H192zM64 464a48 48 0 1 0 0-96 48 48 0 1 0 0 96zm48-208a48 48 0 1 0 -96 0 48 48 0 1 0 96 0z" />
-                        </svg>Productos <i class="ri-arrow-down-s-line dropdown__arrow"></i>
+                        </svg> <a href="./producto.php" class="producto_azul" >Productos</a>  <i class="ri-arrow-down-s-line dropdown__arrow"></i>
                     </div>
 
                     <ul class="dropdown__menu">
